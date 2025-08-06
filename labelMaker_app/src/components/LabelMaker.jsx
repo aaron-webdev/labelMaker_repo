f41304339841f74cdfff209b0e0bdd9b7ba38d31
aaron-webdev <aaronclure.webdev@gmail.com>
2025-08-06 17:44:30 -0400

    async

--- labelMaker_app/src/components/LabelMaker.jsx
+++ labelMaker_app/src/components/LabelMaker.jsx
@@ -14,38 +14,3 @@ export default function LabelMaker()
     )
 }
 
-/*
-<p id=''></p>
-style={{height:'4in', width:'2in'}}
-<section className='m-auto p-auto'>
-<Card className='m-auto p-auto h-100 w-50'>
-<Card.Body className='m-auto p-auto'>
-<Card.Title>Product Name</Card.Title>
-<Card.Text>
-<div>
-<p>
-Product Ingredient list
-</p>
-<p>
-NO PRESERVATIVES. USE OR FREEZE BY BEST BY DATE<br/>
-Crafted by: <b>BAKE THAT DOUGH LLC</b><br/>
-10700 Virginia Pine Way, Suite 106<br/>
-Knoxville, TN 37932<br/>            
-</p>
-<p>
-<Row>
-<Col>
-$X.xx
-</Col>
-<Col>
-Lot Number
-</Col>
-</Row>
-Product Code for Three Rivers Market<br/>
-</p>            
-</div>
-</Card.Text>
-</Card.Body>
-</Card>        
-</section>
-*/
